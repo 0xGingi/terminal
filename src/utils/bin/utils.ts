af61036d73be750319694351350d542ff4f34f05
0xGingi <0xgingi@0xgingi.com>
2022-10-27 20:37:21 -0400

    cleanup

--- src/utils/bin/utils.ts
+++ src/utils/bin/utils.ts
@@ -4,7 +4,7 @@ import * as bin from './index';
 export const help = async (args: string[]): Promise<string> => {
   const commands = Object.keys(bin).sort().join(', ');
 
-  return `Available commands:\n${commands}\n\n[tab]\t trigger completion.\n[ctrl+l] clear terminal.\n[ctrl+c] cancel command.\n\n\n\nCheck Out the 'monero' command!`;
+  return `Available commands:\n${commands}\n\n[tab]\t trigger completion.\n[ctrl+l] clear terminal.\n[ctrl+c] cancel command.`;
 };
 
 export const echo = async (args: string[]): Promise<string> => {
