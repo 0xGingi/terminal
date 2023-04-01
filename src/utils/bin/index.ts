1c083562bb5cb408228726c9c922842adb158c91
0xGingi <0xgingi@0xgingi.com>
2023-04-01 16:30:23 -0400

    cleanup
    Removing bloat commands

--- src/utils/bin/index.ts
+++ src/utils/bin/index.ts
@@ -1,7 +1,4 @@
 export * from './about';
-export * from './projects';
 export * from './social';
 export * from './theme';
 export * from './utils';
-export * from './weather';
-export * from './neofetch';
