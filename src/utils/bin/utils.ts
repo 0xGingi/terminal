ff590d3f34371181204ac0d332e070679db80d43
0xgingi <0xgingi@0xgingi.com>
2023-05-10 21:09:36 -0400

    modified:   .gitignore 	modified:   package.json 	modified:   src/components/input/Input.tsx 	modified:   src/styles/global.css 	deleted:    src/utils/bin/cowsay.ts 	modified:   src/utils/bin/index.ts 	deleted:    src/utils/bin/neofetch.ts 	deleted:    src/utils/bin/projects.ts 	modified:   src/utils/bin/utils.ts 	deleted:    src/utils/bin/weather.ts 	modified:   src/utils/shellProvider.tsx 	modified:   yarn.lock 	.yarn/ 	.yarnrc.yml 	public/assets/img/ 	src/utils/bin/trex.ts

--- src/utils/bin/utils.ts
+++ src/utils/bin/utils.ts
@@ -7,30 +7,10 @@ export const help = async (args: string[]): Promise<string> => {
   return `Available commands:\n${commands}\n\n[tab]\t trigger completion.\n[ctrl+l] clear terminal.\n[ctrl+c] cancel command.`;
 };
 
-export const echo = async (args: string[]): Promise<string> => {
-  return args.join(' ');
-};
-
-export const whoami = async (args: string[]): Promise<string> => {
-  return 'guest';
-};
-
-export const date = async (args: string[]): Promise<string> => {
-  return new Date().toString();
-};
-
 export const email = async (args: string[]): Promise<string> => {
   window.open('0xgingi@0xgingi.com');
 
   return 'Opening mailto:0xgingi@0xgingi.com...';
-};
-
-export const sudo = async (args?: string[]): Promise<string> => {
-  setTimeout(function () {
-    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
-  }, 1000);
-
-  return `Permission denied: unable to run the command '${args[0]}' as root.`;
 };
 
 export const repo = async (args?: string[]): Promise<string> => {
