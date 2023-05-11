0930aede3c5c525518bb4e9f269f3a4a687bee7b
0xgingi <0xgingi@0xgingi.com>
2023-05-11 01:27:23 +0000

    modified:   package.json 	modified:   src/utils/bin/utils.ts 	modified:   yarn.lock

--- src/utils/bin/utils.ts
+++ src/utils/bin/utils.ts
@@ -60,10 +60,10 @@ export const searxngtor = async (args?: string[]): Promise<string> => {
 
   return `Opening My SearxNG Instance Tor Hidden Service. Use Engine Token 'unlockall' to use Google & Brave. Fuck Bots. `;
 };
-export const paste = async (args?: string[]): Promise<string> => {
+export const matrix = async (args?: string[]): Promise<string> => {
   setTimeout(function () {
-    window.open('https://paste.0xgingi.com');
+    window.open('https://element.onionsherpa.com.com');
   }, 1000);
 
-  return `Opening paste.0xgingi.com - E2E Pastebin `;
+  return `Opening Matrix/Element `;
 };
