bb7c4517fd334be3e7a74fbc62b19690e5d6917b
0xgingi <0xgingi@0xgingi.com>
2023-05-16 01:19:56 +0000

    modified:   package.json 	modified:   src/utils/bin/utils.ts 	modified:   yarn.lock

--- src/utils/bin/utils.ts
+++ src/utils/bin/utils.ts
@@ -62,8 +62,24 @@ export const searxngtor = async (args?: string[]): Promise<string> => {
 };
 export const matrix = async (args?: string[]): Promise<string> => {
   setTimeout(function () {
-    window.open('https://element.onionsherpa.com.com');
+    window.open('https://element.onionsherpa.com');
   }, 1000);
 
-  return `Opening Matrix/Element `;
+  return `Opening Matrix HomeServer / Element Client `;
+};
+
+export const libreddit = async (args?: string[]): Promise<string> => {
+  setTimeout(function () {
+    window.open('https://reddit.0xgingi.com');
+  }, 1000);
+
+  return `Opening Libreddit - Privacy Respecting Reddit Frontend `;
+};
+
+export const invidious = async (args?: string[]): Promise<string> => {
+  setTimeout(function () {
+    window.open('https://invidious.0xgingi.com');
+  }, 1000);
+
+  return `Opening Invidious - Privacy Respecting YouTube Frontend `;
 };
